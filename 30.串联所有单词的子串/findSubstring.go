b146b9788f6acdf33e5efd14cf57cdd9be327cb4
hjx19950506 <40848187+hjx19950506@users.noreply.github.com>
2020-05-18 12:30:30 +0800

    30

--- 30.串联所有单词的子串/findSubstring.go
+++ 30.串联所有单词的子串/findSubstring.go
@@ -1,28 +1,47 @@
 package findSubstring
 
+import "reflect"
+
+
+//滑动窗口法
+//用words初始化一个map记录每个单词出现的次数
+//以一个单词长度为步长遍历s，并使用temp记录窗口中单词出现的次数
+//比较m和temp得知当前窗口是否满足要求
 func findSubstring(s string, words []string) []int {
-	if s == "" || words == nil {
-		return nil
-	}
 	res := []int{}
 	sLen := len(s)
-	wordLen := len(words[0])
 	numOfWords := len(words)
-	m := make(map[string]int, numOfWords)
+	if s == "" || numOfWords == 0 || numOfWords * len(words[0]) > sLen {
+		return res
+	}
+	wordLen := len(words[0])
+	windowLen := wordLen * numOfWords
+	m := make(map[string]int)
 	for _, v := range words {
-		if _, ok := m[v]; ok {
-			m[v]++;
-		} else {
-			m[v] = 0;
-		}
+		m[v]++
 	}
 
-	for i := 0; i < wordLen; i++ {
-		for j := i; j + numOfWords * wordLen <  sLen; j += wordLen {
-
-
+	for i := 0; i < wordLen && i <= sLen - windowLen; i++ {
+		temp := make(map[string]int)
+		//先把窗填满，然后做一次比较
+		for j := i; j < i + windowLen; j += wordLen {
+			temp[s[j:j+wordLen]]++
 		}
-
+		if reflect.DeepEqual(m, temp) {
+			res = append(res, i)
+		}
+		//每次移动一步，将窗前面那个单词的次数减一
+		//新进窗的单词加一
+		//然后比较
+		for j := i + wordLen; j <= sLen - windowLen; j += wordLen {
+			if temp[s[j-wordLen:j]]--; temp[s[j-wordLen:j]] == 0 {
+				delete(temp, s[j-wordLen:j])
+			}
+			temp[s[j+windowLen-wordLen:j+windowLen]]++
+			if reflect.DeepEqual(m, temp) {
+				res = append(res, j)
+			}
+		}
 	}
 	return res
 }
--- 30.串联所有单词的子串/findSubstring_test.go
+++ 30.串联所有单词的子串/findSubstring_test.go
@@ -22,12 +22,32 @@ func TestFindSubstring(t *testing.T) {
 			[]string{"word","good","best","word"},
 			[]int{},
 		},
+		{
+			"wordgoodgoodgoodbestword",
+			[]string{"word","good","best","good"},
+			[]int{8},
+		},
+		{
+			"a",
+			[]string{},
+			[]int{},
+		},
+		{
+			"a",
+			[]string{"a", "a"},
+			[]int{},
+		},
+		{
+			"aaaaaa",
+			[]string{"aaa","aaa"},
+			[]int{0},
+		},
 	}
 
 	for k, v := range cases {
 		t.Run(fmt.Sprint("test", k), func(t *testing.T) {
 			got := findSubstring(v.s, v.words)
-			if reflect.DeepEqual(got, v.want) {
+			if !reflect.DeepEqual(got, v.want) {
 				t.Errorf("got %v want %v", got, v.want)
 			}
 		})
