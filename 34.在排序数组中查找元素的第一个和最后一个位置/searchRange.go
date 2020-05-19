44bf7ad271fc0fe334307543f825c53826da25a8
jack_hwang <hjx88@qq.com>
2020-05-19 21:02:34 +0800

    34

--- 34.在排序数组中查找元素的第一个和最后一个位置/searchRange.go
+++ 34.在排序数组中查找元素的第一个和最后一个位置/searchRange.go
@@ -1,7 +1,46 @@
 package searchRange
 
+//编写两个函数
+//分别寻找第一个和最后一个target的索引
+//方式都是二分查找
 func searchRange(nums []int, target int) []int {
-
+	return []int{findFirst(nums, target), findLast(nums, target)}
 }
 
-func
+func findFirst(nums []int, target int) int {
+	numsLen := len(nums)
+	if numsLen == 0 {
+		return -1
+	}
+	l, r := 0, numsLen
+	for l < r {
+		mid := (l + r) / 2
+		if nums[mid] == target && (mid == 0 || nums[mid-1] < target) {
+			return mid
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+	return -1
+}
+
+func findLast(nums []int, target int) int {
+	numsLen := len(nums)
+	if numsLen == 0 {
+		return -1
+	}
+	l, r := 0, numsLen
+	for l < r {
+		mid := (l + r) / 2
+		if nums[mid] == target && (mid == numsLen - 1 || nums[mid+1] > target) {
+			return mid
+		} else if nums[mid] > target {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return -1
+}
--- 34.在排序数组中查找元素的第一个和最后一个位置/searchRange_test.go
+++ 34.在排序数组中查找元素的第一个和最后一个位置/searchRange_test.go
@@ -1,6 +1,10 @@
 package searchRange
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 //searchRange(nums []int, target int) []int
 func TestSearchRange(t *testing.T) {
@@ -19,5 +23,29 @@ func TestSearchRange(t *testing.T) {
 			6,
 			[]int{-1,-1},
 		},
+		{
+			[]int{5,7,7,7,7,10},
+			7,
+			[]int{1,4},
+		},
+		{
+			[]int{7,7,7,7,7,7},
+			7,
+			[]int{0,5},
+		},
+		{
+			[]int{0},
+			0,
+			[]int{0,0},
+		},
+	}
+
+	for k, v := range cases {
+		t.Run(fmt.Sprint("test", k), func(t *testing.T) {
+			got := searchRange(v.nums, v.target)
+			if !reflect.DeepEqual(got, v.want) {
+				t.Errorf("got %v want %v", got, v. want)
+			}
+		})
 	}
 }
