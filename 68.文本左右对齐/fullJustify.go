95bf9b50adb8ede7cad22641e19ee0c60acd7b52
hjx19950506 <40848187+hjx19950506@users.noreply.github.com>
2020-06-07 12:46:14 +0800

    68

--- 68.文本左右对齐/fullJustify.go
+++ 68.文本左右对齐/fullJustify.go
@@ -1,12 +1,16 @@
 package fullJustify
 
+import "strings"
+
+//先确定每一行最多能放那几个单词
+//然后再重排这些单词，填充空格
+//最后一行做特殊处理
 func fullJustify(words []string, maxWidth int) []string {
 	length := len(words)
 	res := make([]string, 0)
 	if length == 0 {
 		return res
 	}
-	rows := make([][]string, 0)
 	for i := 0; i < length; {
 		row := make([]string, 0)
 		row = append(row, words[i])
@@ -19,15 +23,37 @@ func fullJustify(words []string, maxWidth int) []string {
 				break
 			}
 		}
-		rows = append(rows, dump(row))
 		i += len(row)
+		if i == length {
+			temp := row[0]
+			for i := 1; i < len(row); i++ {
+				temp += " " + row[i]
+			}
+			temp += strings.Repeat(" ", maxWidth - len(temp))
+			res = append(res, temp)
+		} else {
+			res = append(res, reorder(row, rowLen, maxWidth))
+		}
 	}
 	return res
 }
 
-//todo rerank
-func dump(src []string) string {
-	dst := make([]string, len(src))
-	copy(dst, src)
+func reorder(src []string, rowLen, maxWidth int) string {
+	dst := src[0]
+	srcLen := len(src)
+	spaceCount := maxWidth - rowLen + srcLen - 1
+	if srcLen == 1 {
+		dst += strings.Repeat(" ", spaceCount)
+		return dst
+	}
+	aveSpace := spaceCount / (srcLen - 1)
+	plusOneSpaceCount := spaceCount - aveSpace * (srcLen - 1)
+	for i := 1; i < srcLen; i++ {
+		if i <= plusOneSpaceCount {
+			dst += " "
+		}
+		dst += strings.Repeat(" ", aveSpace)
+		dst += src[i]
+	}
 	return dst
 }
--- 68.文本左右对齐/fullJustify_test.go
+++ 68.文本左右对齐/fullJustify_test.go
@@ -25,14 +25,14 @@ func TestFullJustify(t *testing.T) {
 		{
 			[]string{"Science","is","what","we","understand","well","enough","to","explain", "to","a","computer.","Art","is","everything","else","we","do"},
 			20,
-			[]string{"Science  is  what we", "understand      well", "enough to explain to", "a  computer.  Art is", "everything  else  we", "do                  "},
+			[]string{"Science  is  what we", "understand      well", "enough to explain to", "a  computer.  Art is", "everything  else  we", "do                  "},
 		},
 	}
 	for k, v := range cases {
 		t.Run(fmt.Sprint("test", k+1), func(t *testing.T) {
 			got := fullJustify(v.words, v.maxWidth)
-			if reflect.DeepEqual(got, v.want) {
-				t.Errorf("got %v want %v", got, v.want)
+			if !reflect.DeepEqual(got, v.want) {
+				t.Errorf("got %#v want %#v", got, v.want)
 			}
 		})
 	}
